9d2619094ccb688cd4f83444aaf4dd97d26ba5eb
Tristan Vermeesch <titivermeesch@gmail.com>
2020-12-20 16:18:34 +0100

    path evaluator router

---
+++ src/routes/pathEvaluatorRouter.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import { pathEvaluate } from '../helpers/netflix-evaluator-helpers.js'
+
+const pathEvaluatorRouter = express.Router()
+
+pathEvaluatorRouter.get('/', async (req, res) => {
+  const paths = [
+    [
+      'videos',
+      80184771,
+      [
+        'title',
+        'episodeCount',
+        'evidence',
+        'maturity',
+        'seasonCount',
+        'synopsis',
+      ],
+    ],
+  ]
+  try {
+    const data = await pathEvaluate('', paths)
+    res.send(data)
+  } catch (e) {
+    console.error(e)
+    res.sendStatus(400)
+  }
+})
+
+export default pathEvaluatorRouter
